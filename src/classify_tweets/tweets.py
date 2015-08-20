994c4c5f86165e9462e79c033865379e1aa697ff
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-20 11:02:01 +0600

    Twitter stream producer based on Tweepy.
    This script subscribes to tweets contained coordinates and some images
    at the same time. Basically it just prints 'media' entities from tweets.

---
+++ src/classify_tweets/tweets.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import, print_function
+
+import argparse
+import ConfigParser
+import logging
+import os
+import time
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+logging.basicConfig()
+logger = logging.getLogger('tweets_producer')
+
+
+class StdOutListener(StreamListener):
+    """ A listener handles tweets are the received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_status(self, status):
+        """
+        Callback when post is received ok
+        """
+        if 'media' in status.entities:
+            message = {
+                'coordinates': status.coordinates,
+                'media': status.entities['media'],
+                'text': status.text,
+                'time': int(time.time())
+
+            }
+            logger.info(message['media'])
+
+    def on_error(self, status):
+        logger.error("Got error: {}".format(status))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true")
+    group.add_argument("-q", "--quiet", action="store_true")
+    parser.add_argument("config", nargs='?', type=str,
+                        help="Path to config file",
+                        default="../../etc/tweets_producer_default.cfg")
+
+    args = parser.parse_args()
+
+    if args.quiet:
+        logger.setLevel(logging.WARNING)
+    elif args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if not os.path.exists(args.config):
+        parser.exit(1,
+                    "Configuration file {} doesn't exist".format(args.config))
+
+    config = ConfigParser.ConfigParser()
+    config.read(args.config)
+
+    l = StdOutListener()
+    auth = OAuthHandler(config.get('twitter', 'consumer_key'),
+                        config.get('twitter', 'consumer_secret'))
+    auth.set_access_token(config.get('twitter', 'access_token'),
+                          config.get('twitter', 'access_token_secret'))
+
+    stream = Stream(auth, l)
+    stream.filter(locations=[-180, -90, 180, 90])
