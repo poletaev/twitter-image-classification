60c830a04d52f364160b105518f1820dbb63591b
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-21 00:52:12 +0600

    Files needed by Celery for classify_tweets package.

---
+++ src/classify_tweets/__init__.py
@@ -0,0 +1,2 @@
+from tasks import *
+from celery_app import app
---
+++ src/classify_tweets/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine("mysql://test:password@localhost:3306/dev?charset=utf8",
+                       convert_unicode=True,
+                       pool_recycle=3600)
+                       #pool_size=10)
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
