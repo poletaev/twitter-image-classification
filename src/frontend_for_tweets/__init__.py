ba00e3a70c4bf0ae326dc689e98b726efed3b1ef
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-21 07:40:36 +0600

    Improve frontend configuration.
    SQLAlchemy configuration and more more precise logging were added.

--- src/frontend_for_tweets/__init__.py
+++ src/frontend_for_tweets/__init__.py
@@ -1,13 +1,24 @@
+import logging
 import os
 
 from flask import Flask
 from flask_googlemaps import GoogleMaps
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from google_map import google_map
 
-def create_app(**kwargs):
+
+def create_app(bind='development', debug=False, default_config=None,
+               mysql_path=None):
+
     app = Flask(__name__)
-    app.config.from_pyfile('../../etc/frontend_default.cfg')
+
+    if default_config is None:
+        app.config.from_pyfile('../../etc/frontend_default.cfg')
+    else:
+        app.config.from_object(default_config)
+
     try:
         env_config = 'FRONTEND_FOR_TWEETS_SETTINGS'
         if os.getenv(env_config) is not None:
@@ -18,9 +29,28 @@ def create_app(**kwargs):
     except Exception as e:
         app.logger.error("Error: {}".format(e.message))
 
-    if 'debug' in kwargs and kwargs['debug'] in (True, False):
-        app.debug = kwargs['debug']
+    # set logger format and logging level
+    logging.basicConfig(format=app.config['LOG_FORMAT'],
+                        level=app.config['LOG_LEVEL'])
+    app.debug_log_format = app.config['LOG_FORMAT']
+
+    # set up database
+    if mysql_path is None:
+        app.engine = create_engine(app.config['SQLALCHEMY_BINDS'][bind],
+                                   convert_unicode=True,
+                                   pool_recycle=3600,
+                                   pool_size=10)
+    else:
+        app.engine = create_engine(mysql_path,
+                                   convert_unicode=True,
+                                   pool_recycle=3600,
+                                   pool_size=10)
+
+    # set debug
+    app.debug = debug
 
     app.register_blueprint(google_map)
+    google_map.logger = app.logger
+
     GoogleMaps(app)
     return app
