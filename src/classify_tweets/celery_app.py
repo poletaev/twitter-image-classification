b25d49f5bed397c5d7dd0b7d781f192e2a232979
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-21 15:09:35 +0600

    Load settings for Celery app using environment variable.

--- src/classify_tweets/celery_app.py
+++ src/classify_tweets/celery_app.py
@@ -1,10 +1,24 @@
 from __future__ import absolute_import
 
+import logging
+import os
 from celery import Celery
 
 app = Celery('tweets')
 
+logger = logging.getLogger(__name__)
+
 app.config_from_object('classify_tweets.worker_default')
+
+try:
+    env_config = 'CLASSIFY_TWEETS_SETTINGS'
+    if os.getenv(env_config) is not None:
+        app.config_from_envvar(env_config)
+    else:
+        logger.warning("Environment variable {} was not set."
+                        .format(env_config))
+except Exception as e:
+    logger.exception("Error: {}".format(e.message))
 
 # Optional configuration
 app.conf.update(
