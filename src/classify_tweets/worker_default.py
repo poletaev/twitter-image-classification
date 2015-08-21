6cbe502b8bb6fb29aea22bf1f001d7403f22c8c6
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-08-21 15:16:09 +0600

    Update Celery config to use caffe_classifier.

--- src/classify_tweets/worker_default.py
+++ src/classify_tweets/worker_default.py
@@ -2,7 +2,8 @@
 BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ('classify_tweets.tasks', 'classify_tweets.db' )
+CELERY_IMPORTS = ('classify_tweets.tasks',
+                  'classify_tweets.db')
 
 # Using the database to store task state and results.
 CELERY_RESULT_BACKEND = 'rpc://'
@@ -10,5 +11,12 @@ CELERY_RESULT_BACKEND = 'rpc://'
 # CELERY_ANNOTATIONS = {'tweets.send_tweet': {'rate_limit': '10/s'}}
 # CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
 
-
 CELERYD_CONCURRENCY = 1
+
+MODEL_DEF_FILE = 'models/bvlc_reference_caffenet/deploy.prototxt'
+PRETRAINED_MODEL_FILE = 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+MEAN_FILE = 'models/bvlc_reference_caffenet/ilsvrc_2012_mean.npy'
+CLASS_LABELS_FILE = 'models/bvlc_reference_caffenet/synset_words.txt'
+BET_FILE = 'models/bvlc_reference_caffenet/imagenet.bet.pickle'
+GPU_MODE = False
+TMP_DIR = '/tmp/d'
